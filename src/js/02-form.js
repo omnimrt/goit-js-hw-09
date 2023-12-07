205b482d191103bd36e416b74ee3989e87fbc6b1
Martin <martinmyhalyna@gmail.com>
2023-12-07 08:45:45 -0500

    deleted listener

--- src/js/02-form.js
+++ src/js/02-form.js
@@ -1,41 +1,39 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const form = document.querySelector('.feedback-form');
-  const localStorageKey = 'feedback-form-state';
+const form = document.querySelector('.feedback-form');
+const localStorageKey = 'feedback-form-state';
 
-  // Checking for saved data in localStorage
-  const savedState = JSON.parse(localStorage.getItem(localStorageKey));
+// Checking for saved data in localStorage
+const savedState = JSON.parse(localStorage.getItem(localStorageKey));
 
-  if (savedState) {
-    // Filling form fields with saved data
-    form.elements.email.value = savedState.email || '';
-    form.elements.message.value = savedState.message || '';
-  }
+if (savedState) {
+  // Filling form fields with saved data
+  form.elements.email.value = savedState.email || '';
+  form.elements.message.value = savedState.message || '';
+}
 
-  // Adding input event listener using event delegation
-  form.addEventListener('input', event => {
-    const formData = {
-      email: form.elements.email.value,
-      message: form.elements.message.value,
-    };
+// Adding input event listener using event delegation
+form.addEventListener('input', event => {
+  const formData = {
+    email: form.elements.email.value,
+    message: form.elements.message.value,
+  };
 
-    // Saving form data to localStorage
-    localStorage.setItem(localStorageKey, JSON.stringify(formData));
-  });
+  // Saving form data to localStorage
+  localStorage.setItem(localStorageKey, JSON.stringify(formData));
+});
 
-  // Adding submit event listener
-  form.addEventListener('submit', event => {
-    event.preventDefault();
+// Adding submit event listener
+form.addEventListener('submit', event => {
+  event.preventDefault();
 
-    // Getting and logging form data
-    const formData = {
-      email: form.elements.email.value,
-      message: form.elements.message.value,
-    };
+  // Getting and logging form data
+  const formData = {
+    email: form.elements.email.value,
+    message: form.elements.message.value,
+  };
 
-    console.log(formData);
+  console.log(formData);
 
-    // Clearing localStorage and form fields
-    localStorage.removeItem(localStorageKey);
-    form.reset();
-  });
+  // Clearing localStorage and form fields
+  localStorage.removeItem(localStorageKey);
+  form.reset();
 });
